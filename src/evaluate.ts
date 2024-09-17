3f4fca38dac88e5bafc2e45c81e78d3596946fb9
~ . ~ <paul.n.wand@gsa.gov>
2024-09-17 07:41:47 -0400

    only use the metaschema for the document in question

--- src/evaluate.ts
+++ src/evaluate.ts
@@ -1,7 +1,7 @@
 import { readFileSync, unlinkSync, writeFileSync } from 'fs';
 import path from 'path';
 import { v4 } from 'uuid';
-import { executeOscalCliCommand, installOscalCli, isOscalCliInstalled } from './commands.js';
+import { detectOscalDocumentType, executeOscalCliCommand, installOscalCli, isOscalCliInstalled } from './commands.js';
 import { Catalog, Profile } from './types.js';
 
 interface EvaulateOptions {
@@ -18,8 +18,21 @@ export async function evaluateMetapathCommand(options:EvaulateOptions): Promise<
 export async function evaluateMetapath(options:EvaulateOptions): Promise<string|undefined> {
 
     if(typeof options.metaschema==='undefined'||options.metaschema===""){
-        options.metaschema = "https://raw.githubusercontent.com/usnistgov/OSCAL/main/src/metaschema/oscal_complete_metaschema.xml"
+      const [documentType,fileFormat]=await detectOscalDocumentType(options.document)
+      const metaschemaMap = {
+        'catalog': 'oscal_catalog_metaschema.xml',
+        'profile': 'oscal_profile_metaschema.xml',
+        'component': 'oscal_component_metaschema.xml',
+        'ssp': 'oscal_ssp_metaschema.xml',
+        'poam': 'oscal_poam_metaschema.xml',
+        'assessment-plan': 'oscal_assessment-plan_metaschema.xml',
+        'assessment-results': 'oscal_assessment-results_metaschema.xml'
     }
+
+    const metaschemaFile = metaschemaMap[documentType] || 'oscal_complete_metaschema.xml'
+     
+    options.metaschema = `https://raw.githubusercontent.com/usnistgov/OSCAL/main/src/metaschema/${metaschemaFile}`
+  }
     let oscalCliInstalled = await isOscalCliInstalled();
   if (!oscalCliInstalled) {
     // If OSCAL CLI is not installed, attempt to install it

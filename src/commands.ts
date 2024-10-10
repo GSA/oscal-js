3ebd4a5124ab8efec5154d661f99ee93bf8b54a4
~ . ~ <paul.n.wand@gsa.gov>
2024-10-10 16:13:58 -0400

    version bump

--- src/commands.ts
+++ src/commands.ts
@@ -9,7 +9,7 @@ import { validateCommand } from './validate.js';
 import { useVersion } from './versions.js';
 
 program
-  .version("2.0.0-rc6")
+  .version("2.0.0-rc7")
   .command('validate [file]')
   .option('-s, --server', 'Use OSCAL server for operations')
   .option('-f, --file <path>', 'Path to the OSCAL document or directory')

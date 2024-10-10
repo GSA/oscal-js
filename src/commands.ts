e43d9bb9f9d107659130b2a135966ece083c85e2
~ . ~ <paul.n.wand@gsa.gov>
2024-10-09 20:31:06 -0400

    run integration test on server

--- src/commands.ts
+++ src/commands.ts
@@ -9,7 +9,7 @@ import { validateCommand } from './validate.js';
 import { useVersion } from './versions.js';
 
 program
-  .version("2.0.0-rc3")
+  .version("2.0.0-rc4")
   .command('validate [file]')
   .option('-s, --server', 'Use OSCAL server for operations')
   .option('-f, --file <path>', 'Path to the OSCAL document or directory')
